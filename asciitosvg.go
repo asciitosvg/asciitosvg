c3c3cdf8f9c93190d32ab4279a1b39d36779491d
Devon H. O'Dell <dho@fastly.com>
2015-07-05 16:03:35 -0700

    Uniform grid width simplifies code

--- asciitosvg.go
+++ asciitosvg.go
@@ -82,17 +82,17 @@ func (c *Canvas) scanBox(wg *sync.WaitGroup, p []Point, row, col, rowInc, colInc
 
 			if rowInc == 0 && colInc == 1 {
 				// Moving right, we can move up or down
-				if row < len(c.grid)-1 && col < len(c.grid[row+1]) && c.grid[row+1][col] == '|' {
+				if row < len(c.grid)-1 && c.grid[row+1][col] == '|' {
 					wg.Add(1)
 					go c.scanBox(wg, p, row+1, col, 1, 0)
 				}
-				if row > p[0].Y && col < len(c.grid[row-1]) && c.grid[row-1][col] == '|' {
+				if row > p[0].Y && c.grid[row-1][col] == '|' {
 					wg.Add(1)
 					go c.scanBox(wg, p, row-1, col, -1, 0)
 				}
 			} else if rowInc == 1 && colInc == 0 {
 				// Moving down, we can move left or right
-				if col < len(c.grid[row])-1 && c.grid[row][col+1] == '-' {
+				if c.grid[row][col+1] == '-' {
 					wg.Add(1)
 					go c.scanBox(wg, p, row, col+1, 0, 1)
 				}
@@ -102,17 +102,17 @@ func (c *Canvas) scanBox(wg *sync.WaitGroup, p []Point, row, col, rowInc, colInc
 				}
 			} else if rowInc == 0 && colInc == -1 {
 				// Moving left, we can move up or down
-				if row > 0 && col < len(c.grid[row-1]) && c.grid[row-1][col] == '|' {
+				if row > 0 && c.grid[row-1][col] == '|' {
 					wg.Add(1)
 					go c.scanBox(wg, p, row-1, col, -1, 0)
 				}
-				if row < len(c.grid)-1 && col < len(c.grid[row+1]) && c.grid[row+1][col] == '|' {
+				if row < len(c.grid)-1 && c.grid[row+1][col] == '|' {
 					wg.Add(1)
 					go c.scanBox(wg, p, row+1, col, -1, 0)
 				}
 			} else if rowInc == -1 && colInc == 0 {
 				// Moving up, we can move left or right
-				if col < len(c.grid[row])-1 && c.grid[row][col+1] == '-' {
+				if c.grid[row][col+1] == '-' {
 					wg.Add(1)
 					go c.scanBox(wg, p, row, col+1, 0, 1)
 				}
@@ -141,7 +141,7 @@ func (c *Canvas) FindBoxes() Boxes {
 		for col, char := range line {
 			// Corners appearing on the last row or column of the
 			// grid do not have enough space to start a new box
-			if row < len(c.grid)-1 && col < len(c.grid[row])-1 && col < len(c.grid[row+1]) {
+			if row < len(c.grid)-1 {
 				// Only consider boxes starting at top-left
 				if isCorner(char) && c.grid[row][col+1] == '-' && c.grid[row+1][col] == '|' {
 					wg.Add(1)
